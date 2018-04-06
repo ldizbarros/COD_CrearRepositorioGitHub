596fabcef78b0f249b3c8b8ea0144e9d0d85dec7
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2018-04-06 09:38:44 +0200

    Metodos hacerCommit y clonarRepositorio

--- src/main/java/usolibreriagithub/MavenProject.java
+++ src/main/java/usolibreriagithub/MavenProject.java
@@ -3,13 +3,26 @@ package usolibreriagithub;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.eclipse.jgit.api.errors.GitAPIException;
 
 public class MavenProject {
 
     public static void main(String[] args) {
+        
+            //        try {
+//            Metodos.crearRepositorio("RepositorioPrueba");
+//        } catch (IOException ex) {
+//            Logger.getLogger(MavenProject.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        
+//        try {
+//            Metodos.clonarRepositorio("https://github.com/ldizbarros/COD_CrearRepositorioGitHub.git", "/home/local/DANIELCASTELAO/ldizbarros/NetBeansProjects/CrearRepositorioGitHub");
+//        } catch (GitAPIException ex) {
+//            Logger.getLogger(MavenProject.class.getName()).log(Level.SEVERE, null, ex);
+//        }
         try {
-            Metodos.crearRepositorio("RepositorioPrueba");
-        } catch (IOException ex) {
+            Metodos.hacerCommit("/home/local/DANIELCASTELAO/ldizbarros/NetBeansProjects/JavaApplication125/.git", "PRUEBA COMMIT");
+        } catch (GitAPIException | IOException ex) {
             Logger.getLogger(MavenProject.class.getName()).log(Level.SEVERE, null, ex);
         }
     } 
--- src/main/java/usolibreriagithub/Metodos.java
+++ src/main/java/usolibreriagithub/Metodos.java
@@ -1,8 +1,16 @@
 package usolibreriagithub;
 
+import java.io.File;
 import java.io.IOException;
+import org.eclipse.jgit.api.AddCommand;
+import org.eclipse.jgit.api.CommitCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.kohsuke.github.GHCreateRepositoryBuilder;
+import org.eclipse.jgit.lib.Repository;
 import org.kohsuke.github.GitHub;
+
 
 public class Metodos {
     
@@ -13,4 +21,27 @@ public class Metodos {
         builder.create();
     }
     
+    public static void clonarRepositorio(String URIrepositorio, String directorio) throws GitAPIException{
+        Git git = Git.cloneRepository()
+            .setURI(URIrepositorio)
+            .setDirectory(new File(directorio))
+            .setCloneAllBranches(true)
+            .call();
+    }
+    
+    public static void hacerCommit(String direcionRepositorio, String fraseCommit) throws IOException, GitAPIException{
+        FileRepositoryBuilder constructorRepositorio = new FileRepositoryBuilder();
+        Repository repositorio = constructorRepositorio.setGitDir(new File(direcionRepositorio))
+                .readEnvironment() // scan environment GIT_* variables
+                .findGitDir() // scan up the file system tree
+                .setMustExist(true)
+                .build();
+        
+        Git git = new Git(repositorio);
+        AddCommand add = git.add();
+        add.addFilepattern(direcionRepositorio).call();
+        
+        CommitCommand commit = git.commit();
+        commit.setMessage(fraseCommit).call();
+    }
 }
